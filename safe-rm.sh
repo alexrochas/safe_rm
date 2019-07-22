f5b28f2a43e0f406e576ee03f85b9e2600d05591
Alex Rocha <arocha@zalando-61672.corp.ad.zalando.net>
2019-07-22 13:22:25 +0200

    feat: add force rm

--- safe-rm.sh
+++ safe-rm.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
+# script path
+SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
+
+while getopts ":f:" option; do
+	case "${option}" in
+		f)
+            echo "Force rm for $OPTARG!"
+            rm $OPTARG
+            exit 0
+            ;;
+	esac
+done
+
+# delete old files (older than 10 days)
+echo "Cleanning old files in $SCRIPTPATH"
+find $SCRIPTPATH/.bin_trash/* -mtime +10d | xargs rm -r
 # show me the money!
-echo $1
-
+echo "Removing $1"
 # create the bin
-mkdir ./.bin_trash
-
+[[ -f $SCRIPTPATH/.bin_trash ]] && mkdir ./.bin_trash
 # copy to a temporary folder
-cp $1 ./.bin_trash
-
+cp $1 $SCRIPTPATH/.bin_trash
 # remove the original
 rm $1
+
